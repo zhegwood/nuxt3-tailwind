e42af09361cc65d37f80409288e9529abbda6152
Zach <zhegwood@gmail.com>
2022-10-19 15:41:54 -0600

    Initial Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  css: ["@/assets/css/main.css"],
+  build: {
+    postcss: {
+      postcssOptions: require("./postcss.config.js"),
+    },
+  },
+});
